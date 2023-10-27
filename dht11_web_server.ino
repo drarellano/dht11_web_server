bc69296e712b9c82a2727afe9c8d704f2d63dbab
darellanus <darellano@utags.edu.mx>
2023-10-26 18:42:34 -0600

    Librerias agregadas

---
+++ dht11_web_server.ino
@@ -0,0 +1,14 @@
+#include <DHT.h>
+#include "WiFi.h"
+#include "EspAsyncWebSrv.h"
+// #include <Adafruit_Sensor.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
